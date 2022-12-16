46ce589555fedb566cc2636eb8cee75172699487
sipuswain <118711973+sipuswain@users.noreply.github.com>
2022-12-16 16:56:52 +0530

    Create 2_1.c

---
+++ assignment_02/2_1.c
@@ -0,0 +1,13 @@
+
+// 1. Write a program to print unit digit of a given number.
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter a no.\n");
+    scanf("%d",&n);
+    printf("unit digit is %d",n%10);
+    return 0;
+}
